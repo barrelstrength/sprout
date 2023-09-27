2855386b2b933e092b07017e9cfbf21989bfb449
Ben Parizek <ben.parizek@gmail.com>
2023-09-26 21:52:54 -0400

    Updates existing migration to improve foreign key handling

--- src/core/migrations/m230904_000000_remove_source_groups_table.php
+++ src/core/migrations/m230904_000000_remove_source_groups_table.php
@@ -3,13 +3,22 @@
 namespace BarrelStrength\Sprout\core\migrations;
 
 use craft\db\Migration;
+use craft\helpers\Db;
 
 class m230904_000000_remove_source_groups_table extends Migration
 {
     public const SOURCE_GROUPS_TABLE = '{{%sprout_source_groups}}';
 
+    public const DATASETS_TABLE = '{{%sprout_datasets}}';
+
     public function safeUp(): void
     {
+        if ($this->getDb()->tableExists(self::DATASETS_TABLE) && $this->getDb()->columnExists(self::DATASETS_TABLE, 'groupId')) {
+            $fkName = Db::findForeignKey(self::DATASETS_TABLE, 'groupId');
+            $this->dropForeignKey($fkName, self::DATASETS_TABLE);
+            $this->dropColumn(self::DATASETS_TABLE, 'groupId');
+        }
+
         $this->dropTableIfExists(self::SOURCE_GROUPS_TABLE);
     }
 
