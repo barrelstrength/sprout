ba6c84b9b19519caa6fa0084758d81eeb0124ae9
Ben Parizek <ben.parizek@gmail.com>
2023-11-04 22:32:44 -0400

    Updates notification event tips

--- assets/dist/assets/transactionalEmail-928f1dbe.js
+++
@@ -1,2 +0,0 @@
-import{m as t}from"./module.esm-9a996e1c.js";window.Alpine=t;const n=i=>({selectedNotificationEvent:i,init(){}});t.data("NotificationEventTip",n);t.start();
-//# sourceMappingURL=transactionalEmail-928f1dbe.js.map
