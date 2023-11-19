ae9d03b130d4509440edeb5d977365147fb9210b
Ben Parizek <ben.parizek@gmail.com>
2023-11-19 12:01:56 -0500

    Updates form resources

--- assets/src/forms/Forms.js
+++ assets/src/forms/Forms.js
@@ -4,9 +4,7 @@ import './forms-ui.scss';
 import '../core/SourceElementRelations';
 import './formbuilder/App';
 import './NewForm';
-import './FormSettings';
 import './Integrations';
-import './IntegrationModal';
 
 import './IntegrationsRelationsTable';
 import '../data-studio/DataSourceRelationsTable';
