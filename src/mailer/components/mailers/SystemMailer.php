ffa207bf882013429fcf0f612c2d26bccf2eb2a7
Ben Parizek <ben.parizek@gmail.com>
2023-11-25 08:36:29 -0500

    Updates send test modal

--- src/mailer/components/mailers/SystemMailer.php
+++ src/mailer/components/mailers/SystemMailer.php
@@ -131,6 +131,7 @@ abstract class SystemMailer extends Mailer implements MailerSendTestInterface
 
         return Craft::$app->getView()->renderTemplate('sprout-module-mailer/_components/mailers/SystemMailer/send-test-fields.twig', [
             'email' => $email,
+            'mailerInstructionsSettings' => $email->getMailerInstructions(),
             'warningMessage' => $warningMessage ?? '',
         ]);
     }
